c02e5ae881542ad41e117c65391e669506af584a
nicolepristin <nicole.pristin@gmail.com>
2018-06-09 14:04:37 -0400

    Add cross origin allowance to user service

--- src/main/java/webdev/services/UserService.java
+++ src/main/java/webdev/services/UserService.java
@@ -10,6 +10,7 @@ import webdev.repositories.UserRepository;
 import javax.servlet.http.HttpSession;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class UserService {
     @Autowired
     UserRepository userRepository;
