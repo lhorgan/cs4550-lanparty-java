d0e603bf2056edcc731e37d053450071082ddd77
nicolepristin <nicole.pristin@gmail.com>
2018-06-09 14:49:27 -0400

    Add method for finding recipes by user

--- src/main/java/webdev/services/RecipeService.java
+++ src/main/java/webdev/services/RecipeService.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import webdev.models.Recipe;
+import webdev.models.User;
 import webdev.repositories.RecipeRepository;
+import webdev.repositories.UserRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +20,9 @@ public class RecipeService {
     @Autowired
     RecipeRepository recipeRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/api/recipe")
     public List<Recipe> findAllRecipes() {
         return (List<Recipe>) recipeRepository.findAll();
@@ -28,6 +33,17 @@ public class RecipeService {
         Optional<Recipe> maybeRecipe = recipeRepository.findById(recipeId);
         if (maybeRecipe.isPresent()) {
             return maybeRecipe.get();
+        }
+        return null;
+    }
+
+    @GetMapping("/api/user/{uid}/recipe")
+    public List<Recipe> findRecipesByUser(@PathVariable("uid") int userId) {
+        Optional<User> maybeUser = userRepository.findById(userId);
+        if (maybeUser.isPresent()) {
+            User user = maybeUser.get();
+            List<Recipe> recipes = user.getRecipes();
+            return recipes;
         }
         return null;
     }
