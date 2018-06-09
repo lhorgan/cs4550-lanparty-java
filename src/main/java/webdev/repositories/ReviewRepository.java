f0e9b69fb92b938e31e39aae642cdb126106685d
nicolepristin <nicole.pristin@gmail.com>
2018-06-09 16:21:55 -0400

    Add review repository

---
+++ src/main/java/webdev/repositories/ReviewRepository.java
@@ -0,0 +1,7 @@
+package webdev.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import webdev.models.Review;
+
+public interface ReviewRepository extends CrudRepository<Review, Integer> {
+}
