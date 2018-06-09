cde556decf691521701a3bed1b4a81e56f5f367a
nicolepristin <nicole.pristin@gmail.com>
2018-06-09 14:22:09 -0400

    Add method for finding all recipes

--- src/main/java/webdev/services/RecipeService.java
+++ src/main/java/webdev/services/RecipeService.java
@@ -1,9 +1,23 @@
 package webdev.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import webdev.models.Recipe;
+import webdev.repositories.RecipeRepository;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class RecipeService {
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    @GetMapping("/api/recipe")
+    public List<Recipe> findAllRecipes() {
+        return (List<Recipe>) recipeRepository.findAll();
+    }
 }
