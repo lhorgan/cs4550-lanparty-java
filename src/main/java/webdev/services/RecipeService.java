41abb5b7b61d506433f4cba04ec54e0fa8f364ac
nicolepristin <nicole.pristin@gmail.com>
2018-06-09 14:23:22 -0400

    Add method for finding a recipe by id

--- src/main/java/webdev/services/RecipeService.java
+++ src/main/java/webdev/services/RecipeService.java
@@ -3,11 +3,13 @@ package webdev.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import webdev.models.Recipe;
 import webdev.repositories.RecipeRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -19,5 +21,14 @@ public class RecipeService {
     @GetMapping("/api/recipe")
     public List<Recipe> findAllRecipes() {
         return (List<Recipe>) recipeRepository.findAll();
+    }
+
+    @GetMapping("/api/recipe/{recipeId}")
+    public Recipe findRecipeById(@PathVariable("recipeId") int recipeId) {
+        Optional<Recipe> maybeRecipe = recipeRepository.findById(recipeId);
+        if (maybeRecipe.isPresent()) {
+            return maybeRecipe.get();
+        }
+        return null;
     }
 }
