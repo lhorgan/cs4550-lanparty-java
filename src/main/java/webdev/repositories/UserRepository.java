48b2655e2293a50e9644906f90693253212b3495
nicolepristin <nicole.pristin@gmail.com>
2018-06-09 13:27:55 -0400

    Add user repository

---
+++ src/main/java/webdev/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package webdev.repositories;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import webdev.models.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    
+}
