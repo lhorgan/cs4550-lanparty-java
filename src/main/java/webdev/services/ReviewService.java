2774f97474a20def6b5d10e3d87105b6933487f3
nicolepristin <nicole.pristin@gmail.com>
2018-06-09 16:27:02 -0400

    Add method to find all reviews

--- src/main/java/webdev/services/ReviewService.java
+++ src/main/java/webdev/services/ReviewService.java
@@ -2,9 +2,13 @@ package webdev.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import webdev.models.Review;
 import webdev.repositories.ReviewRepository;
 import webdev.repositories.UserRepository;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -15,5 +19,8 @@ public class ReviewService {
     @Autowired
     UserRepository userRepository;
 
-
+    @GetMapping("/api/review")
+    public List<Review> findAllReviews() {
+        return (List<Review>) reviewRepository.findAll();
+    }
 }
